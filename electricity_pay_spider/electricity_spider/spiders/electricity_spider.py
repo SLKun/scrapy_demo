c1e9eb43eb102a5bb906541b534282a4cc666ca0
SL <summeslyb@gmail.com>
2017-08-29 17:33:00 +0800

    wait for threadsafe update

--- electricity_pay_spider/electricity_spider/spiders/electricity_spider.py
+++ electricity_pay_spider/electricity_spider/spiders/electricity_spider.py
@@ -4,7 +4,9 @@ import helper.IterHelper
 
 class ElectricitySpider(scrapy.Spider):
     name = "electricity"
-    data = {}
+
+    def fetch_option(self, param):
+        levels = ['programId', 'txtyq', 'txtld']
 
     def update_data(self, response):
         __EVENTTARGET = response.xpath('//input[@name="__EVENTTARGET"]/@value').extract_first()
@@ -18,24 +20,29 @@ class ElectricitySpider(scrapy.Spider):
         self.data['__VIEWSTATE'] = __VIEWSTATE
         self.data['__EVENTVALIDATION'] = __EVENTVALIDATION
 
-
     def start_requests(self):
         self.url = "http://202.114.18.218/main.aspx"
         self.data = {}
         yield scrapy.Request(self.url, callback=self.choose_area)
 
+
     def choose_area(self, response):
         options = []
         for option in response.xpath('//select[@id="programId"]/option'):
-            optionVal = option.xpath('./@value').extract_first()
-            if not "-1" in optionVal:
-                options.append(optionVal)
-        print("Options: " + ",".join(options))
+            option_val = option.xpath('./@value').extract_first()
+            if "-1" not in option_val:
+                options.append(option_val)
+        print("Area Options: " + ",".join(options))
 
         self.update_data(response)
         self.data['__EVENTTARGET'] = 'programId'
-        self.data['programId'] = '东区'
-        yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_building)
+        for option in options:
+            self.data['programId'] = option
+            req = scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_building)
+            req.meta['area'] = option
+            yield req
+        # self.data['programId'] = '东区'
+        # yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_building)
 
     def choose_building(self, response):
         options = []
@@ -43,12 +50,18 @@ class ElectricitySpider(scrapy.Spider):
             optionVal = option.xpath('./@value').extract_first()
             if not "-1" in optionVal:
                 options.append(optionVal)
-        print("Options: " + ",".join(options))
+        print("Building Options of " +  str(response.meta['area']) + ": " + ",".join(options))
 
         self.update_data(response)
         self.data['__EVENTTARGET'] = 'txtyq'
-        self.data['txtyq'] = '南二舍'
-        yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_room)
+        # self.data['txtyq'] = '南二舍'
+        # yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_room)
+        for option in options:
+            self.data['txtyq'] = option
+            req = scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_room)
+            req.meta['area'] = response.meta['area']
+            req.meta['building'] = option
+            yield req
 
     def choose_room(self, response):
         options = []
@@ -56,7 +69,7 @@ class ElectricitySpider(scrapy.Spider):
             optionVal = option.xpath('./@value').extract_first()
             if not "-1" in optionVal:
                 options.append(optionVal)
-        print("Options: " + ",".join(options))
+        print("Room Options of " + response.meta['area'] + "_" + response.meta['building'] + ": " + ",".join(options))
 
         self.update_data(response)
         self.data['__EVENTTARGET'] = ''
