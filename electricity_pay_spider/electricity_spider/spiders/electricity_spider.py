878f976d575cb231a8d3d9e5d6b194692ef3e354
SLKun <summerslyb@gmail.com>
2017-08-29 21:58:47 +0800

    optimize code

--- electricity_pay_spider/electricity_spider/spiders/electricity_spider.py
+++ electricity_pay_spider/electricity_spider/spiders/electricity_spider.py
@@ -5,83 +5,77 @@ import helper.IterHelper
 class ElectricitySpider(scrapy.Spider):
     name = "electricity"
 
-    def fetch_option(self, param):
-        levels = ['programId', 'txtyq', 'txtld']
+    def fetch_option(self, response):
+        # check selected options
+        option_list = []
+        levels = {0: 'programId', 1: 'txtyq', 2: 'txtld', 3: ''}
+        for level in levels.values():
+            xpath_str = '//select[@id="' + level + '"]/option[@selected]/@value'
+            result = response.xpath(xpath_str).extract_first()
+            if result and result != "-1":
+                option_list.append(result)
 
-    def update_data(self, response):
-        __EVENTTARGET = response.xpath('//input[@name="__EVENTTARGET"]/@value').extract_first()
-        __EVENTARGUMENT = response.xpath('//input[@name="__EVENTARGUMENT"]/@value').extract_first()
-        __LASTFOCUS = response.xpath('//input[@name="__LASTFOCUS"]/@value').extract_first()
-        __VIEWSTATE = response.xpath('//input[@name="__VIEWSTATE"]/@value').extract_first()
-        __EVENTVALIDATION = response.xpath('//input[@name="__EVENTVALIDATION"]/@value').extract_first()
-        self.data['__EVENTTARGET'] = __EVENTTARGET
-        self.data['__EVENTARGUMENT'] = __EVENTARGUMENT
-        self.data['__LASTFOCUS'] = __LASTFOCUS
-        self.data['__VIEWSTATE'] = __VIEWSTATE
-        self.data['__EVENTVALIDATION'] = __EVENTVALIDATION
-
-    def start_requests(self):
-        self.url = "http://202.114.18.218/main.aspx"
-        self.data = {}
-        yield scrapy.Request(self.url, callback=self.choose_area)
-
-
-    def choose_area(self, response):
+        # decide the level of options to print
         options = []
-        for option in response.xpath('//select[@id="programId"]/option'):
+        for option in response.xpath('//select[@id="' + levels[len(option_list)] + '"]/option'):
             option_val = option.xpath('./@value').extract_first()
             if "-1" not in option_val:
                 options.append(option_val)
-        print("Area Options: " + ",".join(options))
 
-        self.update_data(response)
-        self.data['__EVENTTARGET'] = 'programId'
-        for option in options:
-            self.data['programId'] = option
-            req = scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_building)
-            req.meta['area'] = option
-            yield req
-        # self.data['programId'] = '东区'
-        # yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_building)
+        # print options
+        print("_".join(option_list) + " Options: " + ",".join(options))
+        return options
 
-    def choose_building(self, response):
-        options = []
-        for option in response.xpath('//select[@id="txtyq"]/option'):
-            optionVal = option.xpath('./@value').extract_first()
-            if not "-1" in optionVal:
-                options.append(optionVal)
-        print("Building Options of " +  str(response.meta['area']) + ": " + ",".join(options))
+    def fetch_data(self, response):
+        data = {}
 
-        self.update_data(response)
-        self.data['__EVENTTARGET'] = 'txtyq'
-        # self.data['txtyq'] = '南二舍'
-        # yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_room)
-        for option in options:
-            self.data['txtyq'] = option
-            req = scrapy.FormRequest(self.url, formdata=self.data, callback=self.choose_room)
-            req.meta['area'] = response.meta['area']
-            req.meta['building'] = option
-            yield req
+        # Data from input area
+        input_list = ['__EVENTTARGET', '__EVENTARGUMENT', '__LASTFOCUS', '__VIEWSTATE', '__EVENTVALIDATION', 'Txtroom',
+                      'TextBox2', 'TextBox3']
+        for input in input_list:
+            input_xpath = '//input[@name="' + input + '"]/@value'
+            result = response.xpath(input_xpath).extract_first()
+            if result:
+                data[input] = result
+            else:
+                data[input] = ''
 
-    def choose_room(self, response):
-        options = []
-        for option in response.xpath('//select[@id="txtld"]/option'):
-            optionVal = option.xpath('./@value').extract_first()
-            if not "-1" in optionVal:
-                options.append(optionVal)
-        print("Room Options of " + response.meta['area'] + "_" + response.meta['building'] + ": " + ",".join(options))
+        # Data from select area
+        select_list = ['programId', 'txtyq', 'txtld']
+        for select in select_list:
+            select_xpath = '//select[@id="' + select + '"]/option[@selected]/@value'
+            result = response.xpath(select_xpath).extract_first()
+            if result and result != "-1":
+                data[select] = result
 
-        self.update_data(response)
-        self.data['__EVENTTARGET'] = ''
-        self.data['txtld'] = '4层'
-        self.data['Txtroom'] = '401'
-        self.data['ImageButton1.x'] = '15'
-        self.data['ImageButton1.y'] = '15'
-        yield scrapy.FormRequest(self.url, formdata=self.data, callback=self.fetch_result)
+        # Fixed Data
+        data['ImageButton1.x'] = '15'
+        data['ImageButton1.y'] = '15'
+        return data
+
+    def start_requests(self):
+        self.url = "http://202.114.18.218/main.aspx"
+        self.param = {
+            'programId': "东区",
+            'txtyq': "南一舍",
+            'txtld': "1层",
+            'Txtroom': "123",
+        }
+        yield scrapy.Request(self.url, callback=self.choose_requests)
+
+    def choose_requests(self, response):
+        data = self.fetch_data(response)
+        levels = ['programId', 'txtyq', 'txtld']
+        for level in levels:
+            if level not in data.keys():
+                data[level] = self.param[level]
+                yield scrapy.FormRequest(self.url, formdata=data, callback=self.choose_requests)
+                return
+        data['Txtroom'] = self.param['Txtroom']
+        yield scrapy.FormRequest(self.url, formdata=data, callback=self.fetch_result)
 
     def fetch_result(self, response):
-        result = {}
-        for option in response.xpath('//table[@id="GridView2"]/tr[td]'):
-            time = option.xpath('./td[2]/text()').re(r'20([0-9-]*) .*')[0]
-            data = option.xpath('./td[1]/text()').extract_first()
+        for column in response.xpath('//table[@id="GridView2"]/tr[td]'):
+            time = column.xpath('./td[2]/text()').re(r'20([0-9-]*) .*')[0]
+            data = column.xpath('./td[1]/text()').extract_first()
             print(time + ": " + data)
