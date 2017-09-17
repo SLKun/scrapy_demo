10981fd16d1bcf465fab06ed81f3c86ae433de3e
SLKun <summerslyb@gmail.com>
2017-09-17 18:17:30 +0800

    merge work 40%

--- university_url_spider/merge.py
+++ university_url_spider/merge.py
@@ -2,25 +2,64 @@ import sys
 import json
 
 
+def print_items(dict):
+    for item in dict.items():
+        print(item)
+
+def tran_list_to_dict(list):
+    dict = {}
+    for item in list:
+        for key in item.keys():
+            dict[key] = item[key]
+    return dict
+
+def merge(A, B):
+    AandB = {}
+    AexB = {}
+    BexA = {}
+    error = {}
+
+    for key in A_dict.keys():
+        if key in B_dict:
+            if A_dict[key] in B_dict[key] or B_dict[key] in A_dict[key]:
+                AandB[key] = A_dict[key]
+            else:
+                error[key] = {A_dict[key], B_dict[key]}
+            B_dict.pop(key)
+        else:
+            AexB[key] = A_dict[key]
+
+    for key in B_dict.keys():
+        BexA[key] = B_dict[key]
+
+    print(len(A_json), len(B_json))
+    print(len(AandB), len(error))
+    print(len(AexB), len(BexA))
+
+    return [result, error]
+
 
 def main():
-    arg_list = ["A_filename", "B_filename", "AandB_filename", "AexB_filename", "BexA_filename"]
-    if len(sys.argv) <= 1:
-        A_filename = "A.json"
-        B_filename = "B.json"
-        AandB_filename = "A+B.json"
-        AexB_filename = "A-B.json"
-        BexA_filename = "B-A.json"
-    else:
-        for i in range(1, len(sys.argv)):
-            setattr(self, "test", "wqqw")
+    params_list = ["A_filename", "B_filename", "result_filename", "error_filename"]
+    params = {
+        "A_filename": "A.json",
+        "B_filename": "B.json",
+        "result_filename": "result.json", 
+        "error_filename": "error.json", 
+    }
+    for i in range(1, len(sys.argv)):
+        params[params_list[i-1]] = sys.argv[i]
 
-    print(vars())
-
-    A = open(A_filename).read()
-    B = open(B_filename).read()
+    A = open(params['A_filename']).read()
+    B = open(params['B_filename']).read()
+    result = open(params_list['result_filename'], "a")
+    error = open(params_list['error_filename'], "a")
     A_json = json.loads(A)
     B_json = json.loads(B)
+    A_dict = tran_list_to_dict(A_json)
+    B_dict = tran_list_to_dict(B_json)
+    
+
 
 if __name__ == '__main__':
     main()
